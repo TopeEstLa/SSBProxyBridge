c9e8a994d820773ec0afd1ab315f4780b79d1d58
OmerBenGera <omerthepro126@gmail.com>
2022-09-24 21:11:37 +0300

    Added sync for player modes that weren't synced before (admin bypass, admin spy, etc)

--- Bukkit/src/main/java/com/bgsoftware/ssbproxybridge/bukkit/listener/PlayersListener.java
+++ Bukkit/src/main/java/com/bgsoftware/ssbproxybridge/bukkit/listener/PlayersListener.java
@@ -3,8 +3,18 @@ package com.bgsoftware.ssbproxybridge.bukkit.listener;
 import com.bgsoftware.ssbproxybridge.bukkit.SSBProxyBridgeModule;
 import com.bgsoftware.ssbproxybridge.bukkit.action.ActionsQueue;
 import com.bgsoftware.ssbproxybridge.bukkit.action.ServerActions;
+import com.bgsoftware.ssbproxybridge.bukkit.bridge.ProxyDatabaseBridge;
+import com.bgsoftware.superiorskyblock.api.data.DatabaseBridge;
+import com.bgsoftware.superiorskyblock.api.data.DatabaseFilter;
 import com.bgsoftware.superiorskyblock.api.events.AttemptPlayerSendMessageEvent;
+import com.bgsoftware.superiorskyblock.api.events.PlayerToggleBlocksStackerEvent;
+import com.bgsoftware.superiorskyblock.api.events.PlayerToggleBorderEvent;
 import com.bgsoftware.superiorskyblock.api.events.PlayerToggleBypassEvent;
+import com.bgsoftware.superiorskyblock.api.events.PlayerToggleFlyEvent;
+import com.bgsoftware.superiorskyblock.api.events.PlayerToggleSpyEvent;
+import com.bgsoftware.superiorskyblock.api.events.PlayerToggleTeamChatEvent;
+import com.bgsoftware.superiorskyblock.api.objects.Pair;
+import com.bgsoftware.superiorskyblock.api.wrappers.SuperiorPlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -46,19 +56,35 @@ public class PlayersListener implements Listener {
     }
 
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
-    public void onToggleBypass(PlayerToggleBypassEvent event) {
-        Player player = event.getReceiver().asPlayer();
+    public void onToggleBlocksStacker(PlayerToggleBlocksStackerEvent event) {
+        trySyncServers(event.getPlayer(), new Pair<>("blocks_stacker", !event.getPlayer().hasBlocksStackerEnabled()));
+    }
 
-        if (player != null)
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onToggleTeamChat(PlayerToggleTeamChatEvent event) {
+        trySyncServers(event.getPlayer(), new Pair<>("team_chat", !event.getPlayer().hasTeamChatEnabled()));
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onToggleBypass(PlayerToggleBypassEvent event) {
+        trySyncServers(event.getPlayer(), new Pair<>("admin_bypass", !event.getPlayer().hasBypassModeEnabled()));
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onToggleSpy(PlayerToggleSpyEvent event) {
+        trySyncServers(event.getPlayer(), new Pair<>("admin_spy", !event.getPlayer().hasAdminSpyEnabled()));
+    }
+
+    private void trySyncServers(SuperiorPlayer superiorPlayer, Pair<String, Object> data) {
+        DatabaseBridge databaseBridge = superiorPlayer.getDatabaseBridge();
+
+        if (!(databaseBridge instanceof ProxyDatabaseBridge))
             return;
 
-        event.setCancelled(true);
-
-        Object[] args = new Object[event.getArgumentsLength()];
-        for (int i = 0; i < args.length; ++i)
-            args[i] = event.getArgument(i);
-
-        ServerActions.sendMessage(event.getReceiver().getUniqueId(), event.getMessageType(), args);
+        ProxyDatabaseBridge proxyDatabaseBridge = (ProxyDatabaseBridge) databaseBridge;
+        proxyDatabaseBridge.customOperation("players",
+                DatabaseFilter.fromFilter("uuid", superiorPlayer.getUniqueId().toString()),
+                new Pair[]{data});
     }
 
 }
